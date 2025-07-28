ab5102f88a63aaf8b52a8eaa25ff49c52a022a58
TayH203 <TayDacHo2005@gmail.com>
2025-07-28 16:49:53 -0400

    SillyStory: Commit 5

--- Assignment4/SillyStory/main.js
+++ Assignment4/SillyStory/main.js
@@ -8,11 +8,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = "It was 94 fahrenheit outside, so :insertx: "
-+ "went for a walk. When they got to :inserty:, " 
-+ "they stared in horror for a few moments, then :insertz:. " 
-+ "Bob saw the whole thing, but was not surprised — :insertx: " 
-+ "weighs 300 pounds, and it was a hot day.";
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
 const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 
@@ -23,18 +19,30 @@ const insertZ; = ["spontaneously combusted", "melted into a puddle on the sidewa
 randomize.addEventListener('click', result);
 
 function result() {
-
+  let newStory = storyText;
+  
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+  
+  newStory = newStory
+			 .replaceAll(":insertx:", xItem)
+             .replaceAll(":inserty:", yItem)
+             .replaceAll(":insertz:", zItem);
+  
   if(customName.value !== '') {
     const name = customName.value;
-
+	newStory = newStory.replace("Bob", name);
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
+    const weight = ${Math.round(300 / 14)} "stone";
+    const temperature = ${Math.round((94-32) * (5/9))} "centigrade";
+	newStory = newStory
+			   .replace("300 pounds", weight)
+			   .replace("94 fahrenheit", temperature);
   }
-
-  story.textContent = ;
+   	
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
