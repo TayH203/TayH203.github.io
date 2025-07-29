9fae2616607d4a4e7f3f70ff7d29e59490e38d56
TayH203 <TayDacHo2005@gmail.com>
2025-07-28 23:53:17 -0400

    ImageGallery: Commit 3
    declaring alt and images

--- Assignment4/ImageGallery/main.js
+++ Assignment4/ImageGallery/main.js
@@ -5,8 +5,17 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const picture = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg',];
 
 /* Declaring the alternative text for each image file */
+const altPicture = 
+{
+	'pic1.jpg': 'Human Eye';
+	'pic2.jpg': 'Amazing Snow Art';
+	'pic3.jpg': 'Flowers';
+	'pic4.jpg': 'Ancient Egypt';
+	'pic5.jpg': 'Moth leaf';
+};
 
 /* Looping through images */
 
