8df18849f7d452595a5ef9b4421fe5ad8654dee3
TayH203 <TayDacHo2005@gmail.com>
2025-07-29 00:20:34 -0400

    ImageGallery: Commit 5

--- Assignment4/ImageGallery/main.js
+++ Assignment4/ImageGallery/main.js
@@ -5,16 +5,23 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const picture = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const picture = 
+[
+	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic1.jpg', 
+	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic2.jpg', 
+	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic3.jpg', 
+	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic4.jpg', 
+	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic5.jpg'
+];
 
 /* Declaring the alternative text for each image file */
 const altPicture = 
 {
-	'pic1.jpg': 'Human Eye',
-	'pic2.jpg': 'Amazing Snow Art',
-	'pic3.jpg': 'Flowers',
-	'pic4.jpg': 'Ancient Egypt',
-	'pic5.jpg': 'Moth leaf'
+	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic1.jpg': "Human Eye", 
+	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic2.jpg': "Snow", 
+	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic3.jpg': "Flowers", 
+	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic4.jpg': "Egypt", 
+	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic5.jpg':  "Butterfly"
 };
 
 /* Looping through images */
@@ -38,3 +45,20 @@ newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () =>
+{
+	const btnClass = btn.getAttribute('class');'
+	
+	if (btnClass === 'dark')
+	{
+		btn.setAttribute('class', 'light');
+		btn.textContent = 'Lighten';
+		overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+	}
+	else
+	{
+		btn.setAttribute('class'. 'dark');
+		btn.textContent = 'Darken';
+		overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+	}
+});
