0666c7630bbdbbcc30fbc8030683d9fbed537aa0
TayH203 <TayDacHo2005@gmail.com>
2025-07-29 00:01:37 -0400

    ImageGallery: Commit 4
    Update and add looping

--- Assignment4/ImageGallery/main.js
+++ Assignment4/ImageGallery/main.js
@@ -5,19 +5,32 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const picture = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg',];
+const picture = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 const altPicture = 
 {
-	'pic1.jpg': 'Human Eye';
-	'pic2.jpg': 'Amazing Snow Art';
-	'pic3.jpg': 'Flowers';
-	'pic4.jpg': 'Ancient Egypt';
-	'pic5.jpg': 'Moth leaf';
+	'pic1.jpg': 'Human Eye',
+	'pic2.jpg': 'Amazing Snow Art',
+	'pic3.jpg': 'Flowers',
+	'pic4.jpg': 'Ancient Egypt',
+	'pic5.jpg': 'Moth leaf'
 };
 
 /* Looping through images */
+for (const image of picture) 
+{
+	const newImage = document.createElement('img');
+	newImage.setAttribute('src', picture);
+	newImage.setAttribute('alt', altText[picture]);		
+	thumbBar.appendChild(newImage); 
+	
+	newImage.addEventListener('click', () => 
+	{
+		displayedImage.setAttribute('src', newImage.getAttribute('src'));
+		displayedImage.setAttribute('alt', newImage.getAttribute('alt'));
+	}
+}
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
