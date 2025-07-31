8eba5bd4bc05949e48135db15fad718dab98db9b
TayH203 <TayDacHo2005@gmail.com>
2025-07-30 23:19:16 -0400

    ImageGallery: Commit 6
    Updated and finish imageGallery

--- Assignment4/ImageGallery/main.js
+++ Assignment4/ImageGallery/main.js
@@ -5,49 +5,44 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const picture = 
+const pictures = 
 [
-	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic1.jpg', 
-	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic2.jpg', 
-	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic3.jpg', 
-	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic4.jpg', 
-	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic5.jpg'
+	'images/pic1.jpg', 
+	'images/pic2.jpg', 
+	'images/pic3.jpg', 
+	'images/pic4.jpg', 
+	'images/pic5.jpg'
 ];
 
 /* Declaring the alternative text for each image file */
-const altPicture = 
+const altTexts = 
 {
-	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic1.jpg': "Human Eye", 
-	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic2.jpg': "Snow", 
-	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic3.jpg': "Flowers", 
-	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic4.jpg': "Egypt", 
-	'file:///C:/Users/Tayak/OneDrive/Documents/GitHub/TayHo/Assignment4/ImageGallery/images/pic5.jpg':  "Butterfly"
+	'images/pic1.jpg': 'Human Eye', 
+	'images/pic2.jpg': 'Snow', 
+	'images/pic3.jpg': 'Flowers', 
+	'images/pic4.jpg': 'Egypt', 
+	'images/pic5.jpg': 'Butterfly'
 };
 
 /* Looping through images */
-for (const image of picture) 
+for (let i = 0; i < pictures.length; i++) 
 {
 	const newImage = document.createElement('img');
-	newImage.setAttribute('src', picture);
-	newImage.setAttribute('alt', altText[picture]);		
+	newImage.setAttribute('src', pictures[i]);
+	newImage.setAttribute('alt', altTexts[pictures[i]]);		
 	thumbBar.appendChild(newImage); 
 	
 	newImage.addEventListener('click', () => 
 	{
 		displayedImage.setAttribute('src', newImage.getAttribute('src'));
 		displayedImage.setAttribute('alt', newImage.getAttribute('alt'));
-	}
+	});
 }
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
 btn.addEventListener('click', () =>
 {
-	const btnClass = btn.getAttribute('class');'
+	const btnClass = btn.getAttribute('class');
 	
 	if (btnClass === 'dark')
 	{
@@ -57,7 +52,7 @@ btn.addEventListener('click', () =>
 	}
 	else
 	{
-		btn.setAttribute('class'. 'dark');
+		btn.setAttribute('class', 'dark');
 		btn.textContent = 'Darken';
 		overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
 	}
