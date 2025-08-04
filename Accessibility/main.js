79154659b169d19dc41fada3ad97b74e95b77274
TayH203 <TayDacHo2005@gmail.com>
2025-08-04 16:31:51 -0400

    Accessibility: Commit 5
    Finished draft

--- Accessibility/main.js
+++ Accessibility/main.js
@@ -5,6 +5,8 @@ const commentWrapper = document.querySelector('.comment-wrapper');
 
 commentWrapper.style.display = 'none';
 
+showHideBtn.setAttribute('aria-expended', 'false');
+
 showHideBtn.onclick = function() {
   let showHideText = showHideBtn.textContent;
   if(showHideText === 'Show comments') {
@@ -15,6 +17,13 @@ showHideBtn.onclick = function() {
     commentWrapper.style.display = 'none';
   }
 };
+
+// Allows accessible for show/hide comment when pressing the enter key
+showHideBtn.onkeydown = (e) => {
+	if (e.key === "Enter") {
+		showHideBtn.click();
+	}
+}
 
 // functionality for adding a new comment via the comments form
 
